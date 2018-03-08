5672751a80552451d74286afc50e295c175a514f
saadmanrafat <saadman@outlook.com>
2018-03-09 05:03:50 +0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README') as f:
+    long_description = f.read()
+
+setup(
+    name='imgur-scraper',
+    version='',
+    description='',
+    license='MIT',
+    long_description=long_description,
+    author='Saadman Rafat',
+    author_email='saadmanhere@gmail.com',
+    url='',
+    package='',
+    install_requires=[
+        'requests-html'
+    ],
+    scripts=[],
+    classifiers=[]
+)
+
+
---
+++ test.py
@@ -0,0 +1,4 @@
+from imgur_scraper import get_viral_posts_from
+
+for k in get_viral_posts_from(date="31/05/17"):
+    print(k)
