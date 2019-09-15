c0c6653a3bc3ad850af97435232d4a4a2f1a5b7b
saadmanrafat <saadman@outlook.com>
2019-09-15 15:36:53 +0600

    making README.md PyPI friendly

--- setup.py
+++ setup.py
@@ -1,82 +1,26 @@
-import io
 import os
-import sys
-from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
 
-# Package meta-data.
 NAME = "imgur_scraper"
-DESCRIPTION = "Retrieve Imgur's viral posts from a given date (it goes back years)."
+DESCRIPTION = "Scrape years of Imgur's data without any authentication."
 URL = "https://github.com/saadmanrafat/imgur-scraper"
 EMAIL = "saadmanhere@gmail.com"
-
 AUTHOR = "Saadman Rafat"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.9"
-
-# What packages are required for this module to be executed?
+VERSION = "0.1.10"
 REQUIRED = ["requests-html", "requests"]
 
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPi via Twine…")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags…")
-        os.system("git tag v{0}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
     name=NAME,
-    version=about["__version__"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=VERSION,
     description=DESCRIPTION,
     author=AUTHOR,
     author_email=EMAIL,
@@ -90,15 +34,9 @@ setup(
     include_package_data=True,
     license="MIT",
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    # $ setup.py publish support.
-    cmdclass={"upload": UploadCommand},
 )
