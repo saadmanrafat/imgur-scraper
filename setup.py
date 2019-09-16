c3f86444423432963497853deffaaebab071d2e3
saadmanrafat <saadman@outlook.com>
2019-09-16 16:26:03 +0600

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/saadmanrafat/imgur-scraper"
 EMAIL = "saadmanhere@gmail.com"
 AUTHOR = "Saadman Rafat"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.13"
+VERSION = "0.1.14"
 REQUIRED = ["requests-html", "requests"]
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
